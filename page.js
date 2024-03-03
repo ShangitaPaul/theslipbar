ef9b285099148074dc41c2ac72e476b40b7b11b0
Shangita Paul <shangita.paul@gmail.com>
2024-03-02 20:55:21 -0800

    Working Draft V1 (no React)

---
+++ page.js
@@ -0,0 +1,32 @@
+// Toggle mobile navigation links
+function toggleMobileNav() {
+    const mobileNavLinks = document.querySelector('.mobile-nav-links');
+    mobileNavLinks.style.display = (mobileNavLinks.style.display === 'flex') ? 'none' : 'flex';
+}
+  
+// // Mobile navigation links
+// function toggleMobileNav() {
+//   document.body.classList.toggle('mobile-nav-active');
+// }
+
+// Corrected JavaScript function for toggling mobile navigation
+
+// // function toggleMobileNav() {
+//   document.body.classList.toggle('mobile-nav-active');
+// }
+
+  
+//   // Toggle mobile navigation links
+// document.addEventListener('DOMContentLoaded', function () {
+//     const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+//     const mobileNavLinks = document.querySelector('.mobile-nav-links');
+  
+//     mobileNavToggle.addEventListener('click', function () {
+//       if (mobileNavLinks.style.display === 'flex' || mobileNavLinks.style.display === '') {
+//         mobileNavLinks.style.display = 'none';
+//       } else {
+//         mobileNavLinks.style.display = 'flex';
+//       }
+//     });
+//   });
+  
